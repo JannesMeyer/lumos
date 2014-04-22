aff3456fa49f396c1ffc6791301e85e4174b308e
Jannes <jannes.meyer@gmail.com>
2014-04-22 21:36:54 +0200

    Directory listings and file views work correctly

--- directory.js
+++ directory.js
@@ -19,6 +19,7 @@ function startsWith(str, search) {
 	// no type checking or null checking
 	return str.length >= search.length && str.slice(0, search.length) === search;
 }
+// Stat all of these fuckers
 function endsWith(str, search) {
 	// no type checking or null checking
 	return str.length >= search.length && str.slice(str.length - search.length) === search;
@@ -28,62 +29,158 @@ function endsWith(str, search) {
 let Directory = {
 
 	settings: {
+		brandName: 'Notes',
 		root: path.join(__dirname, 'markdown'),
 		mdSuffix: '.md'
 	},
 
-	viewDirectory: function(res, next, dirPath) {
-		let settings = this.settings;
-		let indexFile = 'index' + settings.mdSuffix;
+	getIndexFilename: function() {
+		let self = this;
+		return 'index' + self.settings.mdSuffix;
+	},
 
-		// Read directory contents
+	getAbsolutePath: function(relativePath) {
+		let self = this;
+		return path.join(self.settings.root, relativePath);
+	},
+
+	stripMdSuffix: function(name) {
+		let self = this;
+		return path.basename(name, self.settings.mdSuffix);
+	},
+
+	parseDirectory: function(dirPath) {
+		let self = this;
+
 		let dirContent;
-		FS.readDir(dirPath)
-		.then(function(_dirContent) {
-			// Remove hidden files/directoies
-			dirContent = _dirContent.filter(function(name) {
-				return !startsWith(name, '.');
+		return FS.readDir(dirPath)
+		.then(function(names) {
+			// Create item objects
+			dirContent = names.map(function(name) {
+				return {
+					name: name,
+					extension: path.extname(name).toLowerCase(),
+					path: path.join(dirPath, name),
+					isHidden: startsWith(name, '.')
+				};
 			});
 
-			// let promises = [];
-			// for (let i = 0; i < dirContent.length; ++i) {
-			// 	promises.push(FS.stat(path.join(dirPath, dirContent[i])));
-			// }
-			return Promise.all(dirContent.map(function(name) {
-				return FS.stat(path.join(dirPath, name));
+			// Stat all of these fuckers
+			return Promise.all(dirContent.map(function(item) {
+				return FS.stat(item.path);
 			}));
-
-
-			// && name !== indexFile
-			// dirContent = dirContent.map(function(file) {
-			// 	return path.basename(file, settings.mdSuffix);
-			// });
-
-			// let data = {};
-			// data.title = path.basename(dirPath);
-			// data.path = dirPath;
-			// data.files = dirContent;
-			// res.render('directory', data);
 		})
 		.then(function(stats) {
-			res.end(util.inspect(stats));
-			// let files = [];
-			// let directories = [];
-		})
-		.catch(function(err) {
-			next(err);
+			dirContent.forEach(function(it, i) {
+				// Get corresponding stat object
+				let stat = stats[i];
+
+				// Append item objects
+				let isDir = stat.isDirectory();
+				it.isDirectory = isDir;
+				// Add other properties for directories/files
+				if (isDir) {
+					it.basename = it.name;
+					it.link = it.name + '/';
+				} else {
+					let basename = self.stripMdSuffix(it.name);
+					it.basename = basename;
+					it.link = basename;
+				}
+			});
+			return dirContent;
 		});
 	},
 
-	viewFile: function(res, next, filePath) {
-		let settings = this.settings;
+	filterDirectory: function(items) {
+		let self = this;
+		let indexFilename = self.getIndexFilename();
 
-		FS.readFile(filePath, { encoding: 'utf-8' })
+		return items.filter(function(item) {
+			return !item.isHidden
+				// Filter files
+				&& (item.isDirectory || (!item.isDirectory
+					&& item.name !== indexFilename && item.extension === self.settings.mdSuffix));
+		});
+	},
+
+	viewDirectory: function(res, relativePath) {
+		let self = this;
+		let absolutePath = self.getAbsolutePath(relativePath);
+
+		// Read directory contents
+		return self.parseDirectory(absolutePath)
+		.then(function(items) {
+			let filtered = self.filterDirectory(items);
+
+			let basename = path.basename(relativePath);
+			let data = {
+				title: basename.length !== 0 ? basename : self.settings.brandName,
+				items: filtered
+			};
+			res.render('directory', data);
+		});
+	},
+
+	viewFile: function(res, relativePath) {
+		let self = this;
+		let absolutePath = self.getAbsolutePath(relativePath);
+
+		return FS.readFile(absolutePath, { encoding: 'utf-8' })
 		.then(function(content) {
 			let data = {};
-			data.title = path.basename(filePath, settings.mdSuffix);
+			data.title = path.basename(relativePath, self.settings.mdSuffix);
 			data.content = marked(content)
 			res.render('document', data);
+		});
+	},
+
+	handleRequest: function(req, res, next) {
+		let self = this;
+		let relativePath = decodeURIComponent(req.path);
+		let absolutePath = self.getAbsolutePath(relativePath);
+
+		// Make sure the sandbox isn't left
+		if (!startsWith(absolutePath, self.settings.root)) {
+			let err = new Error('Bad Request');
+		    err.status = 400;
+		    next(err);
+		}
+
+		// text/html; charset=utf-8
+		// Checking for direct match
+		FS.stat(absolutePath)
+		.then(function(stat) {
+			if (stat.isDirectory()) {
+				// The URL standard only allows / as a path separator
+				// Make sure directory requests end with a slash
+				if (!endsWith(relativePath, '/')) {
+					res.redirect(301, relativePath + '/');
+					return;
+				}
+
+				return self.viewDirectory(res, relativePath);
+			} else {
+				return self.viewFile(res, relativePath);
+			}
+		}, function(err) {
+			let relativePathMd = relativePath + self.settings.mdSuffix;
+			let absolutePathMd = absolutePath + self.settings.mdSuffix;
+
+			// Checking for [path].md
+			return FS.stat(absolutePathMd)
+			.then(function(stat) {
+				if (stat.isDirectory()) {
+					return new Error('Found directory instead of file');
+				}
+
+				return self.viewFile(res, relativePathMd);
+			}, function(err) {
+				// Definitely nothing found
+				err = new Error('Not Found');
+				err.status = 404;
+				next(err);
+			});
 		})
 		.catch(function(err) {
 			next(err);
@@ -92,51 +189,6 @@ let Directory = {
 
 };
 
-
-
-// Find path
 module.exports = function(req, res, next) {
-	let settings = Directory.settings;
-	let absPath = path.join(settings.root, req.path);
-	let absPathMd = absPath + settings.mdSuffix;
-
-	// Make sure the sandbox isn't left
-	if (!startsWith(absPath, settings.root)) {
-		let err = new Error('Bad Request');
-	    err.status = 400;
-	    next(err);
-	}
-
-	// text/html; charset=utf-8
-	// Checking for directory
-	FS.stat(absPath)
-	.then(function(stat) {
-		// TODO: endswith / redirect 301
-		if (stat.isDirectory()) {
-			// The URL standard only allows / as a path separator
-			if (!endsWith(absPath, '/')) {
-				// Redirect
-				res.redirect(301, req.path + '/');
-				return;
-			}
-
-			return Directory.viewDirectory(res, next, absPath);
-		} else if (stat.isFile()) {
-			return Directory.viewFile(res, next, absPath);
-		}
-	})
-	.catch(function(err) {
-		// Checking for path.md
-		FS.stat(absPathMd)
-		.then(function(stat) {
-			if (stat.isFile()) {
-				return Directory.viewFile(res, next, absPathMd);
-			}
-		})
-		.catch(function(err) {
-			error = new Error('Not Found');
-			error.status = 404;
-			next(error);
-		});
-	});
+	Directory.handleRequest(req, res, next);
 };
