b25355a12f0d747ec7ccbd83c0e0a653722457a4
Jannes <jannes.meyer@gmail.com>
2014-07-07 12:14:11 +0200

    Small code improvement on gulp-jstransform

--- gulp/gulp-jstransform.js
+++ gulp/gulp-jstransform.js
@@ -4,31 +4,28 @@ var path = require('path');
 var jstransform = require('jstransform');
 var visitors = require('../mytransform').visitorList;
 
-module.exports = function(options) {
+var PLUGIN_NAME = 'gulp-jstransfrom';
+
+module.exports = function() {
 	return through.obj(function(file, encoding, callback) {
 		if (file.isNull()) {
 			this.push(file); // pass along
-			return callback();
-		}
-		if (file.isStream()) {
-			this.emit('error', new Error('gulp-jstransfrom: Streaming not supported'));
-			return callback();
-		}
+		} else if (file.isStream()) {
+			throw new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported');
+		} else if (file.isBuffer()) {
+			try {
+				var es5 = jstransform.transform(visitors, file.contents.toString());
+			} catch(err) {
+				err.plugin = file.relative;
+				this.emit('error', err);
+				return callback();
+			}
 
-		var es5;
-		var str = file.contents.toString();
-
-		try {
-			es5 = jstransform.transform(visitors, str);
-		} catch(err) {
-			err.plugin = file.relative;
-			this.emit('error', err);
-			return callback();
+			file.contents = new Buffer(es5.code);
+			file.path = gutil.replaceExtension(file.path, '.js');
+			this.push(file);
 		}
 
-		file.contents = new Buffer(es5.code);
-		file.path = gutil.replaceExtension(file.path, '.js');
-		this.push(file);
 		callback();
 	});
 };
