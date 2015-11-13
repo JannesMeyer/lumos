347d78c46c7dc68a82413551cff4306a60ee1d9e
Jannes <jannes.meyer@gmail.com>
2015-11-13 22:49:45 +0000

    Make docstring prettier

--- src/lib/debounce.js
+++ src/lib/debounce.js
@@ -1,6 +1,8 @@
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds.
+/**
+ * Returns a function, that, as long as it continues to be invoked, will not
+ * be triggered. The function will be called after it stops being called for
+ * N milliseconds.
+ */
 function debounce(fn, wait, hash) {
 	var timeouts = {};
 
