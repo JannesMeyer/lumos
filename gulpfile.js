b279c46137b3058e967c020fa55b0cb67a1f22dc
Jannes <jannes.meyer@gmail.com>
2015-11-13 22:40:30 +0000

    Remove default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,5 @@
-#!/usr/bin/env node --harmony
-/*
-	Rather than manage one giant configuration file responsible
-	for creating multiple tasks, each task has been broken out into
-	its own file in `gulpDir`.
+#!/usr/bin/env node
 
-	To add a new task, simply add a new task file to `gulpDir`.
-*/
-require('./gulp');
 var gulp = require('gulp');
 
-// Default task
-gulp.task('default', function() {
-	console.log('\nAvailable tasks:');
-	for (var task in gulp.tasks) {
-		if (task === 'default') {
-			continue;
-		}
-		console.log(' • ' + task);
-	}
-	console.log('');
-});
+require('./gulp');
