2529263bcda91a68eb038837326067782e3039f5
Jannes <jannes.meyer@gmail.com>
2015-11-14 19:38:40 +0000

    Remove polyfills for String#startsWith and String#endsWith

--- src/lib/polyfills.js
+++
@@ -1,9 +0,0 @@
-// no type checking or null checking
-String.prototype.startsWith = function(search) {
-	return this.length >= search.length && this.slice(0, search.length) === search;
-}
-
-// no type checking or null checking
-String.prototype.endsWith = function(search) {
-	return this.length >= search.length && this.slice(this.length - search.length) === search;
-}
