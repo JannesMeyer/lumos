ec0cec631e8b47b9a65fc9acadcc8d5c138d6537
Jannes <jannes.meyer@gmail.com>
2015-11-21 01:51:22 +0000

    Refactor cmd-search
    - The search command can now print errors on its own (remove debug.js)
    - The main directory is now configured in package.json
    - Move search-spotlight’s content to cmd-search

--- src/cmd-search.js
+++ src/cmd-search.js
@@ -1,23 +1,58 @@
 import { execFile } from 'child_process';
-// import minimist from 'minimist';
-import search from './lib/search-spotlight';
-import debug from './lib/debug';
+import debug from 'debug';
+import { config } from '../package.json';
 
+var log = debug('lumos:search');
+
+/**
+ * Search command
+ */
 export default function cmd(args) {
-  // var argv = minimist(args);
-  // if (argv._.length === 0) {
-  //  throw new Error('Not enough arguments');
-  // }
-
-  var directory = '/Users/jannes/Dropbox/Notes';
-
-  search(directory, args)
-  .then(results => {
+  search(config.directory, args).then(results => {
     results.slice(0, 10).forEach(result => {
       console.log(result);
     });
-    console.log();
-    console.timeEnd('command');
-  })
-  .catch(debug);
+  }).catch(log);
+}
+
+
+
+/**
+ * Search files in directory using `mdfind`
+ */
+function search(directory, terms) {
+  // directory must not end in a slash
+  if (directory.endsWith('/')) {
+    directory = directory.replace(/\/+$/, '');
+  }
+
+  return new Promise(function(resolve, reject) {
+    execFile('mdfind', ['-onlyin', directory, buildQuery(terms)], (err, out) => {
+      if (err) {
+        return reject(err);
+      }
+      if (out === '') {
+        return resolve([]);
+      }
+      resolve(out.trim().split('\n')
+                 .map(result => result.slice(directory.length)));
+    });
+  });
+}
+
+/**
+ * Build query for `mdfind`
+ */
+function buildQuery(terms) {
+  // var query = 'kMDItemContentType == net.daringfireball.markdown';
+  // terms.forEach((term, i) => {
+  //  term = term.replace(/["']/g, '\\$&');
+  //  var isLast = (i === terms.length - 1);
+  //  var wildcard = isLast ? '*' : '';
+  //  query += ` && (kMDItemDisplayName == "${term}${wildcard}"cdwt || kMDItemTextContent == "${term}${wildcard}"cdwt)`;
+  // });
+  // return query;
+
+  terms = terms.join(' ').replace(/["']/g, '\\$&');
+  return `kMDItemContentType == net.daringfireball.markdown && (** = "${terms}*"cdwt)`;
 }
